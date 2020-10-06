a4f4afe15873800bd282fef34bfe2204cc0cea5a
Your Name <norbusamphel@Norbus-MacBook-Pro.local>
2020-10-06 10:02:17 -0500

    just need a click listener

--- client.js
+++ client.js
@@ -1,1 +1,33 @@
 console.log('Here are all the available people:', people);
+
+$(document).ready(onReady);
+
+function onReady() {
+    console.log('JQ loaded');
+
+    // append idvs for all in people array
+   appendPeople();
+   pickRandomPerson();
+};// end of onReady
+
+function appendPeople() {
+    people.forEach(function(person){
+        console.log("person", person);
+    $('#githubPeople').append(`
+        <div>
+            <img src="https://github.com/${person.githubUsername}.png?size=250" alt="Profile image of ${person.name}">
+        </div>
+    `)
+   })// end of loop
+}// end of appendPeople
+
+function pickRandomPerson(){
+   let randomNum = randomNumber(0, people.length - 1);
+   console.log(randomNum);
+
+   $('#person').append(people[randomNum].name);
+}//end of pickRandomPerson
+
+function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+}//end of randomNumber
